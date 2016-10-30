f48f7cf5af52e28e4f94fcfd7545d2bce71aa51c
Volodymyr Lomako <vlomako@ukr.net>
2016-10-30 12:37:36 +0200

    Snake's head pic added

--- gamelogic.js
+++ gamelogic.js
@@ -1,8 +1,9 @@
-
 //Напрямки руху. Можливо, колись додатуться розширення швидко/повільно ліворуч/праворуч
 const [LEFT, RIGHT, FORWARD] = [0, 1, 2];
+const [RUNNING, PAUSED, GAMEOVER] = [0, 1, 2];
 
 const
+    VIEWBOX = { X1:12, X2:497, Y1:12, Y2:347 }; // where the snake's head center can be
     PI = Math.PI,
     PRECISION = 1, // К-ть знаків після коми у шляхах SVG
     TICK_INTERVAL = 50, //ms
@@ -11,14 +12,17 @@ const
     ANGULAR_VELOCITY = VELOCITY/TURN_RADIUS,
     GROWTH = 10, // Приріст довжини в тіках від одного проковтнутого зайця
     START_LENGTH = 20, // Початкова довжина гадюки в тіках
-    MUST_GROWTH = 80, // На скільки тіків змія може вирости на початку гри
-    START_ANGLE = 0.5, // Початковий кут, куди повзе змія
-    START_X = 250, //Початкові координати хвоста гадюки
-    START_Y = 175;
+    MUST_GROWTH = 69, // На скільки тіків змія може вирости на початку гри
+    START_ANGLE = -0.5, // Початковий кут, куди повзе змія
+    START_X = 150, //Початкові координати хвоста гадюки
+    START_Y = 100;
 
-
-const [SHIFT_KEY, CTRL_KEY, ALT_KEY, LEFT_KEY,
-    UP_KEY, RIGHT_KEY, DOWN_KEY] = [16,17,18,37,38,39,40];
+// ========== GLOBALS ==========
+let snake;
+let currentDirection = FORWARD;
+let currentState = GAMEOVER;
+let snakeBody = document.getElementById('snakebody');
+let snakeHead = document.getElementById('head');
 
 class LineSegment {
     constructor(x0, y0, angle) {
@@ -83,6 +87,9 @@ class ArcSegment {
         this.xN = this.ox + TURN_RADIUS * Math.cos(this.angleN);
         this.yN = this.oy - TURN_RADIUS * Math.sin(this.angleN);
         this.drawPath();
+        if (this.angleN-this.angle0 > 1.7*PI) {
+            setState(GAMEOVER);
+        }
     }
     cutTail () {
         this.ticks -= 1;
@@ -97,7 +104,7 @@ class ArcSegment {
 class Snake {
     constructor () {
         this.direction = FORWARD;
-        this.growth = MUST_GROWTH,
+        this.growth = MUST_GROWTH;
         this.segs = [new LineSegment(START_X, START_Y, START_ANGLE)];
         this.segs[0].ticks = START_LENGTH - 1;
         this.nextTick(FORWARD);
@@ -128,67 +135,102 @@ class Snake {
                 this.segs.push(lastseg);
             }
         }
+        this.collisionCheck();
     }
 
     getPath () { //Повертає параметр "d" для SVG-path
         let path0 = `M${this.segs[0].xN.toFixed(PRECISION)},${this.segs[0].yN.toFixed(PRECISION)} `;
         return this.segs.reduce((prev, seg)=> prev+seg.path, path0);
     }
-}
 
-let snake = new Snake();
-
-/*
-* KEYBOARD MODULE
-* *********************************************************************/
-
-var currentDirection = FORWARD;
-
-let leftKeyDown = false;
-let rightKeyDown = false;
-
-function calcDirection() {
-    if (leftKeyDown) {
-        currentDirection = LEFT;
-    } else if (rightKeyDown) {
-        currentDirection = RIGHT;
-    } else {
-        currentDirection = FORWARD;
+    collisionCheck () {
+        let { X1, X2, Y1, Y2 } = VIEWBOX;
+        let [x, y] = [this.segs[0].xN, this.segs[0].yN];
+        if (!(X1<x && x<X2 && Y1<y && y<Y2)) {
+            setState(GAMEOVER);
+        }
     }
 }
 
-function keyDownHandler(event) {
-    var keyCode = event.keyCode;
-    if(keyCode==LEFT_KEY) {
-        leftKeyDown = true;
-        calcDirection();
-    } else if (keyCode==RIGHT_KEY) {
-        rightKeyDown = true;
-        calcDirection();
+// ========== KEYBOARD MODULE ==========
+(() => {
+// const [SHIFT_KEY, CTRL_KEY, ALT_KEY, LEFT_KEY, UP_KEY, RIGHT_KEY, DOWN_KEY] = [16,17,18,37,38,39,40];
+    const [PAUSE_KEY, SPACE_KEY, LEFT_KEY, RIGHT_KEY] = [19, 32, 37,39];
+
+    let leftKeyDown = false;
+    let rightKeyDown = false;
+
+    function calcDirection() {
+        if (leftKeyDown) {
+            currentDirection = LEFT;
+        } else if (rightKeyDown) {
+            currentDirection = RIGHT;
+        } else {
+            currentDirection = FORWARD;
+        }
     }
-}
 
-function keyUpHandler(event) {
-    var keyCode = event.keyCode;
-    if(keyCode==LEFT_KEY) {
-        leftKeyDown = false;
-        calcDirection();
-    } else if (keyCode==RIGHT_KEY) {
-        rightKeyDown = false;
-        calcDirection();
+    function keyDownHandler(event) {
+        var keyCode = event.keyCode;
+        if(keyCode==LEFT_KEY) {
+            leftKeyDown = true;
+            calcDirection();
+        } else if (keyCode==RIGHT_KEY) {
+            rightKeyDown = true;
+            calcDirection();
+        }
     }
-}
 
-document.addEventListener("keydown", keyDownHandler);
-document.addEventListener("keyup", keyUpHandler);
+    function keyUpHandler(event) {
+        var keyCode = event.keyCode;
+        switch (keyCode) {
+            case LEFT_KEY:
+                leftKeyDown = false;
+                calcDirection();
+                break;
+            case RIGHT_KEY:
+                rightKeyDown = false;
+                calcDirection();
+                break;
+            case PAUSE_KEY:
+            case SPACE_KEY:
+                if (currentState==RUNNING) {
+                    setState(PAUSED);
+                } else {
+                    setState(RUNNING);
+                }
 
-/* ****************************************
-  END OF KEYBOARD MODULE
- */
+        }
+    }
+    document.addEventListener("keydown", keyDownHandler);
+    document.addEventListener("keyup", keyUpHandler);
+})(); //  END OF KEYBOARD MODULE
 
-snakeBody = document.getElementById('snakebody');
-
-setInterval(() => {
-    snake.nextTick(currentDirection);
+function drawSnake() {
     snakeBody.setAttribute('d', snake.getPath());
-}, TICK_INTERVAL);
+    let head = snake.segs[0];
+    let angle = 180 - head.getAngle()*180/PI;
+    snakeHead.setAttribute('transform', `translate(${head.xN},${head.yN}) rotate(${angle})`);
+}
+
+const setState = (() => {
+    var timerId;
+    return state => {
+        if (state == currentState) { return; }
+        if (state == RUNNING) {
+            if (currentState == GAMEOVER) {
+                snake = new Snake();
+            }
+            timerId=setInterval(() => {
+                snake.nextTick(currentDirection);
+                drawSnake();
+            }, TICK_INTERVAL);
+        } else {
+            clearInterval(timerId);
+        }
+        currentState = state;
+    }
+})();
+
+snake = new Snake();
+setState(RUNNING);
